14564195f15528ed68048457c8fd8af90a5354a8
Kevin <ktrebing@uni-osnabrueck.de>
2016-12-03 17:46:12 +0100

    additional functionality for retrieval

--- GUI.py
+++ GUI.py
@@ -26,6 +26,7 @@ def queryClick():
     ##### Open QueryWindow #####
     queryWindow = Toplevel()
     queryWindow.title("Query")
+    queryWindow.geometry("300x200")
 
     ####### Select Search options ##########
     kulturOptions = [
@@ -43,10 +44,13 @@ def queryClick():
         "Milben"
     ]
     befall = StringVar(queryWindow)
-    befall.set(befallOptions[0])
+    # befall.set(befallOptions[0])
+    befall.set("-")
     befallDropdown = OptionMenu(queryWindow, befall, *befallOptions)
     befallDropdown.configure(state=DISABLED)
-    befallCheckbox = Checkbutton(master=queryWindow, text="Befall", command=lambda: swapActive(befallDropdown))
+    befallCheckbox = Checkbutton(master=queryWindow, text="Befall",
+                                 command=lambda: checkedBefall(befallDropdown, kultur))
+
 
     # searchOpts = searchOptions(master=queryWindow)
     querySearchButton = Button(master=queryWindow, text="Search", command=searchQueryClick)
@@ -63,25 +67,22 @@ def queryClick():
     # querySearchButton.pack(side=BOTTOM)
 
 
+def checkedBefall(field, kultur):
+    # get possible Befall
+    swapActive(field)
+    print()
 
 def swapActive(field):
-    if field["state"] == NORMAL:
-        field.configure(state="disabled")
-    else:
-        field.configure(state="normal")
-
+    field.configure(state="disabled") if field["state"] == NORMAL else field.configure(state="normal")
 
 def searchQueryClick():
     print("searching")
+
 
 mainWindow = Tk()
 mainWindow.configure(background="black")
 mainWindow.title("Crop Planner")
 mainWindow.geometry("400x240")
-mainButtonWidth = 320
-mainButtonHeight = 80
-
-
 
 manageCropButton = Button(master=mainWindow, text="Manage Crop", command=manageCropClick)
 queryButton = Button(master=mainWindow, text="Get Info about regulations", command=queryClick)
@@ -89,8 +90,6 @@ queryButton = Button(master=mainWindow, text="Get Info about regulations", comma
 
 # manageCropButton.grid(row=0, rowspan=2,columnspan=4, sticky=N)
 # queryButton.grid(row=3, rowspan=2, sticky=N)
-# manageCropButton.place(x=40, y=20, width=mainButtonWidth, height=mainButtonHeight)
-# queryButton.place(x=40, y=120, width=mainButtonWidth, height=mainButtonHeight)
 manageCropButton.pack(fill=BOTH, padx=10, pady=10)
 queryButton.pack(fill=BOTH, padx=10, pady=10)
 
--- retrieval.py
+++ retrieval.py
@@ -11,7 +11,7 @@ import numpy as np
 import pandas
 
 
-def retrieve(culture=None,disease=None):    
+def retrieveSchutzmittel(culture=None, disease=None):
     # load database 
     database = pandas.read_pickle('database_python2.p')
 
@@ -23,30 +23,47 @@ def retrieve(culture=None,disease=None):
         # if no disease is given return all applicable products
         if(disease==None):
             # find all products that can be applied to selected culture
-            product_list = data['Name'].tolist()
+            product_list = list(set(data['Name'].tolist()))
             return product_list
             
         else:
             disease_data = database[database['Schadorganismus'].str.contains(disease)]
-            products_to_disease = disease_data['Name'].tolist()
+            products_to_disease = list(set(disease_data['Name'].tolist()))
             return products_to_disease
-            
+
+    # nothing given --> output all
     else:
-        all_products = database['Name'].tolist()
+        all_products = list(set(database['Name'].tolist()))
         return all_products
-            
-            
+
+
+def retrieveBefall(culture=None):
+    # load database
+    database = pandas.read_pickle('database_python2.p')
+
+    if (culture == None):
+        product_list = list(set(database['Schadorganismus'].tolist()))
+        flattened = [val for sublist in product_list for val in sublist]
+        product_list = list(set(flattened))
+        return product_list
+        # find all data connected to the selected culture
+
+    else:
+        data = database[database['Kultur'].str.contains(culture)]
+        # befall = list(set(data['Schadorganismus'].tolist()))
+        befall = data['Schadorganismus'].apply(lambda x: x.split(', ')).tolist()
+        flattened = [val for sublist in befall for val in sublist]
+        befall = list(set(flattened))
+        return befall
+
+
 # input: culture (e.g. Kartoffel)
-culture = input("Welche Pflanzenkultur pflanzen Sie an? ")
-disease = input("Welchen Befall haben Ihre Pflanzen? ")
+# culture = input("Welche Pflanzenkultur pflanzen Sie an? ")
+# disease = input("Welchen Befall haben Ihre Pflanzen? ")
 
-product_list = retrieve(culture,disease)
-print(product_list)
-
-    
-                
-    
-
+# product_list = retrieveSchutzmittel(culture, disease)
+# product_list = retrieveBefall("Futterrübe")
+# print(product_list)
 
 #print(data)
 

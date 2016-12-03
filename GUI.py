5af9db3670badec1dd989ee326bba0cd50a89ae4
Kevin <ktrebing@uni-osnabrueck.de>
2016-12-03 20:43:31 +0100

    GUI works with classes now

--- GUI.py
+++ GUI.py
@@ -1,23 +1,56 @@
 from tkinter import *
 import retrieval
 
-# class searchOptions:
-#     def __init__(self, master):
-#         self.master = master
-#         self.frame = Tk.frame(self.master)
-#         kulturOptions = [
-#             "Hafer",
-#             "Roggen",
-#             "Kartoffeln"
-#         ]
-#         kultur = StringVar(queryWindow)
-#         kultur.set(kulturOptions[0])
-#         kulturDropdown = OptionMenu(queryWindow, kultur, *kulturOptions)
-#         kulturLabel = Label(master=queryWindow, text="Kultur")
-#
-#         self.kulturCheckbox.pack(side="left")
-#         self.kulturDropdown.pack(side="right")
+class searchOptions:
+    def __init__(self, master):
+        self.master = master
+        # self.frame = Tk.frame(self.master)
+        ####### Select Search options ##########
+        self.kultur = StringVar(master)
+        self.kultur.set("Futterrübe")
+        self.kulturOptions = retrieval.retrieveCultures()  # get Cultures from Database
+        self.kulturDropdown = OptionMenu(master, self.kultur, *self.kulturOptions, command=self.updateBefall)
+        self.kulturLabel = Label(master=master, text="Kultur")
 
+        self.befall = StringVar(master)
+        self.befall.set("")
+        self.befallDropdown = OptionMenu(master, self.befall, [])
+        self.befallDropdown.configure(state=DISABLED)
+        self.checkvar = IntVar()
+        self.befallCheckbox = Checkbutton(master=master, text="Befall", variable=self.checkvar,
+                                     command=lambda: self.checkedBefall())
+
+        self.kulturLabel.grid(row=0, column=1)
+        self.kulturDropdown.grid(row=0, column=3)
+        self.befallCheckbox.grid(row=1, column=1)
+        self.befallDropdown.grid(row=1, column=3)
+
+    def checkedBefall(self):
+        # get possible Befall
+        if self.checkvar.get():         # if checked
+            befallListe = retrieval.retrieveBefall(self.kultur.get())
+            self.befallDropdown['menu'].delete(0, 'end')
+            for choice in befallListe:
+                self.befallDropdown["menu"].add_command(label=choice, command=lambda v=choice: self.befall.set(v))
+            self.befall.set(self.befallDropdown["menu"].entrycget(0, "label"))
+            self.setActive(self.befallDropdown)
+        else:                           # unchecked
+            self.befall.set("")
+            self.setInactive(self.befallDropdown)
+
+    def setActive(self, field):
+        field.configure(state="normal")
+
+    def setInactive(self, field):
+        field.configure(state="disable")
+
+    def updateBefall(self, val):
+        if self.checkvar.get():     # if befall is checked
+            befallListe = retrieval.retrieveBefall(val)
+            self.befallDropdown['menu'].delete(0, 'end')
+            for choice in befallListe:
+                self.befallDropdown["menu"].add_command(label=choice, command=lambda v=choice: self.befall.set(v))
+            self.befall.set(self.befallDropdown["menu"].entrycget(0, "label"))
 
 def manageCropClick():
     ##### Open ManageCropWindow #####
@@ -29,43 +62,19 @@ def queryClick():
     queryWindow.title("Query")
     queryWindow.geometry("500x200")
 
-    ####### Select Search options ##########
-    kultur = StringVar(queryWindow)
-    kultur.set("Futterrübe")
-    kulturOptions = retrieval.retrieveCultures()                        # get Cultures from Database
-    kulturDropdown = OptionMenu(queryWindow, kultur, *kulturOptions)
-    kulturLabel = Label(master=queryWindow, text="Kultur")
 
-
-    befall = StringVar(queryWindow)
-    befall.set("")
-    befallDropdown = OptionMenu(queryWindow, befall, [])
-    befallDropdown.configure(state=DISABLED)
-    befallCheckbox = Checkbutton(master=queryWindow, text="Befall",
-                                 command=lambda: checkedBefall(befallDropdown, kultur.get(), befall))
-
-
-    # searchOpts = searchOptions(master=queryWindow)
+    searchOpts = searchOptions(master=queryWindow)
     querySearchButton = Button(master=queryWindow, text="Search", command=searchQueryClick)
 
     ##### arrangements #####
-    kulturLabel.grid(row=0, column=1)
-    kulturDropdown.grid(row=0, column=3)
-    befallCheckbox.grid(row=1, column=1)
-    befallDropdown.grid(row=1, column=3)
+    # kulturLabel.grid(row=0, column=1)
+    # kulturDropdown.grid(row=0, column=3)
+    # befallCheckbox.grid(row=1, column=1)
+    # befallDropdown.grid(row=1, column=3)
+    # searchOpts.grid(row=0, column=0)
     querySearchButton.grid(row=5, column=2)
 
 
-def checkedBefall(field, kultur, befall):
-    # get possible Befall
-    befallListe = retrieval.retrieveBefall(kultur)
-    field['menu'].delete(0, 'end')
-    for choice in befallListe:
-        field["menu"].add_command(label=choice, command=lambda v=choice: befall.set(v))
-    swapActive(field)
-
-def swapActive(field):
-    field.configure(state="disabled") if field["state"] == NORMAL else field.configure(state="normal")
 
 def searchQueryClick():
     print("searching")

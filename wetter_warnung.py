ee91c6bace99b2823ef3b207b8ad48a57b2e7843
Judith <jschepers@uos.de>
2016-12-04 14:11:57 +0000

    Wettercheck

---
+++ wetter_warnung.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  4 12:23:50 2016
+
+@author: judith
+""" 
+
+import geoCodeWeather
+import pandas
+
+def wetter_check():
+    # get weather forecast for the next week
+    geoCodeWeather.wetterbericht('','Hasbergen')
+    daten = pandas.read_csv('test.csv')
+    
+    # check if temperature exceeds limit 
+    check = daten[(daten['temp']<5) | (daten['temp']>25)]
+    
+    if(check.empty):
+        return "Die Temperatur innerhalb der nächsten Woche liegt im empfohlenen Bereich."
+    
+    else:
+        temperaturen = check['temp'].tolist()
+        minimum = min(temperaturen)
+        maximum = max(temperaturen)
+        if(minimum<5):
+            temp_info = str("Die Tiefsttemperatur innerhalb der nächsten Woche beträgt "+str(minimum)+"°C")
+        elif(maximum>25):
+            temp_info = str("Die Höchsttemperatur innerhalb der nächsten Woche beträgt "+str(maximum)+"°C")
+        else:
+            temp_info = ""
+        warnung = "Warnung: Spritzeinsätze bei großer Hitze oder tiefen Temperaturen sind zu vermeiden. "
+        return warnung+temp_info
